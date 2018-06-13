99dbe22a64e75f72d5cf9d2df9e3477b78a65534
Connie Chi <conniejchi@berkeley.edu>
2018-06-13 14:17:03 -0700

    Test functionality of Array in NumbersActivity.

--- app/src/main/java/com/example/android/chineselearner/NumbersActivity.java
+++ app/src/main/java/com/example/android/chineselearner/NumbersActivity.java
@@ -2,6 +2,7 @@ package com.example.android.chineselearner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +10,22 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        String [] numArr = new String[10];
+
+        numArr[0] = "zero";
+        numArr[1] = "one";
+        numArr[2] = "two";
+        numArr[3] = "three";
+        numArr[4] = "four";
+        numArr[5] = "five";
+        numArr[6] = "six";
+        numArr[7] = "seven";
+        numArr[8] = "eight";
+        numArr[9] = "nine";
+
+        for (int i = 0 ; i < 10; i++) {
+            Log.v("ColorsActivity", "word at index " + i + " is " +  numArr[i]);
+        }
     }
 }
