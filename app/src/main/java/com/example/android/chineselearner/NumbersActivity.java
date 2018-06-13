42037146ebdbfa2aa19b4b4854ad82ca8a1d63ae
Connie Chi <conniejchi@berkeley.edu>
2018-06-13 16:18:26 -0700

    Updated to ListView to recycle views

--- app/src/main/java/com/example/android/chineselearner/NumbersActivity.java
+++ app/src/main/java/com/example/android/chineselearner/NumbersActivity.java
@@ -3,11 +3,15 @@ package com.example.android.chineselearner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -35,13 +39,15 @@ public class NumbersActivity extends AppCompatActivity {
 //            Log.v("ColorsActivity", "word at index " + i + " is " + numArr.get(i));
 //        }
 
-        LinearLayout root = (LinearLayout) this.findViewById(R.id.rootView);
-        //View linearLayout =  findViewById(R.id.info);
-        for (int i= 0; i < numArr.size(); i++) {
-            TextView zero = new TextView(this);
-            zero.setText(numArr.get(i));
-            root.addView(zero);
-        }
+//        ListView root = (ListView) this.findViewById(R.id.rootView);
+//        for (int i= 0; i < numArr.size(); i++) {
+//            TextView zero = new TextView(this);
+//            zero.setText(numArr.get(i));
+//            root.addView(zero);
+//        }
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, numArr);
+        ListView listView = (ListView) findViewById(R.id.rootView);
+        listView.setAdapter(itemsAdapter);
 
     }
 }
