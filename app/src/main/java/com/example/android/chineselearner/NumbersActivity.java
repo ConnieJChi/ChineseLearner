ba66474e2b786ec4598c9aee007545d3c7f74825
Connie Chi <conniejchi@berkeley.edu>
2018-06-13 15:48:47 -0700

    Display numbers under numbers activity.

--- app/src/main/java/com/example/android/chineselearner/NumbersActivity.java
+++ app/src/main/java/com/example/android/chineselearner/NumbersActivity.java
@@ -3,6 +3,11 @@ package com.example.android.chineselearner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -11,21 +16,33 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String [] numArr = new String[10];
 
-        numArr[0] = "zero";
-        numArr[1] = "one";
-        numArr[2] = "two";
-        numArr[3] = "three";
-        numArr[4] = "four";
-        numArr[5] = "five";
-        numArr[6] = "six";
-        numArr[7] = "seven";
-        numArr[8] = "eight";
-        numArr[9] = "nine";
+        ArrayList<String> numArr = new ArrayList<String>();
 
-        for (int i = 0 ; i < 10; i++) {
-            Log.v("ColorsActivity", "word at index " + i + " is " +  numArr[i]);
+        numArr.add("zero");
+        numArr.add("one");
+        numArr.add("two");
+        numArr.add("three");
+        numArr.add("four");
+        numArr.add("five");
+        numArr.add("six");
+        numArr.add("seven");
+        numArr.add("eight");
+        numArr.add("nine");
+        numArr.add("ten");
+
+//        for (int i = 0; i <= 10; i++) {
+//            Log.v("ColorsActivity", "word at index " + i + " is " + numArr.get(i));
+//        }
+
+        LinearLayout root = (LinearLayout) this.findViewById(R.id.rootView);
+        //View linearLayout =  findViewById(R.id.info);
+        for (int i= 0; i < numArr.size(); i++) {
+            TextView zero = new TextView(this);
+            zero.setText(numArr.get(i));
+            root.addView(zero);
         }
+
     }
 }
+
