bde5d8694970462ddca21a783cb10b0a9303cfe9
Connie Chi <conniejchi@berkeley.edu>
2018-06-13 22:41:57 -0700

    Added colors.

--- app/src/main/java/com/example/android/chineselearner/ColorsActivity.java
+++ app/src/main/java/com/example/android/chineselearner/ColorsActivity.java
@@ -3,6 +3,10 @@ package com.example.android.chineselearner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -11,6 +15,21 @@ public class ColorsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
 
+        ArrayList<Word> colors = new ArrayList<>();
+
+        colors.add(new Word("blue", "蓝色"));
+        colors.add(new Word("yellow", "黄色"));
+        colors.add(new Word("red", "红色"));
+        colors.add(new Word("orange","橙色"));
+        colors.add(new Word("green", "绿色"));
+        colors.add(new Word("purple", "紫色"));
+        colors.add(new Word("white", "白色"));
+        colors.add(new Word("gray", "灰色"));
+        colors.add(new Word("black", "黑色"));
+
+        WordAdapter colorsAdapt = new WordAdapter(this, colors);
+        ListView listView = findViewById(R.id.layout);
+        listView.setAdapter(colorsAdapt);
 
     }
 }
