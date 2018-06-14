6999066312490ad3dc221ba67bfb4a9173e050aa
Connie Chi <conniejchi@berkeley.edu>
2018-06-13 22:13:59 -0700

    Cleaned up code.

--- app/src/main/java/com/example/android/chineselearner/NumbersActivity.java
+++ app/src/main/java/com/example/android/chineselearner/NumbersActivity.java
@@ -2,16 +2,9 @@ package com.example.android.chineselearner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
-
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -35,16 +28,10 @@ public class NumbersActivity extends AppCompatActivity {
         numArr.add(new Word("nine", "九"));
         numArr.add(new Word("ten", "十"));
 
-//        WordAdapter flavorAdapter = new WordAdapter(this, numArr);
-//
-//        // Get a reference to the ListView, and attach the adapter to the listView.
-//        ListView listView = (ListView) findViewById(R.id.rootView);
-//        listView.setAdapter(flavorAdapter);
-
         WordAdapter adapter = new WordAdapter(this, numArr);
 
-        ListView listView = (ListView) findViewById(R.id.rootView);
-
+        // Get a reference to the ListView, and attach the adapter to the listView.
+        ListView listView = findViewById(R.id.rootView);
         listView.setAdapter(adapter);
     }
 }
